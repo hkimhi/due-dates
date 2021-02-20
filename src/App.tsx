4ba1db68d9cca2fa9e09dc515e94f9f322aed479
Hod Kimhi <hodkimhi@gmail.com>
2021-02-20 01:01:49 -0800

    Created table to display assignments to user

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,38 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import "./styles.css";
 
 function App() {
-  const [courses ,setCourses] = useState(Object);
+  const [courses, setCourses] = useState(Object);
 
   useEffect(() => {
-    fetch('/data').then(res => res.json()).then(data => {
-      setCourses(data);
-    })
+    fetch("/data")
+      .then((res) => res.json())
+      .then((data) => {
+        setCourses(data);
+      });
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        {Object.keys(courses).map(function(key) {
-          if(courses[key])
-          {
+      <table className="assignments-table">
+        <tr>
+          <th>Name</th>
+          <th>Due Date</th>
+          <th>Source</th>
+        </tr>
+
+        {Object.keys(courses).map(function (key) {
+          return Object.keys(courses[key]).map(function (secondKey) {
             return (
-              <li>{key} - {courses[key].length}</li>
-              
-            )
-          }
-          return null
+              <tr>
+                <td>{courses[key][secondKey].name}</td>
+                <td>{courses[key][secondKey].due_at_date}</td>
+                <td>Canvas</td>
+              </tr>
+            );
+          });
         })}
-      </header>
-    </div>
+      </table>
   );
 }
 
