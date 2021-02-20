37cac42e0ea08e9198ba6a9280ce4c99b457cce1
Hod Kimhi <hodkimhi@gmail.com>
2021-02-19 21:51:05 -0800

    Added flask backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +26,8 @@ function App() {
         >
           Learn React
         </a>
+
+        <p>Current time: {currentTime}.</p>
       </header>
     </div>
   );
